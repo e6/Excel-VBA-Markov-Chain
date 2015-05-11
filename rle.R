d61a691e2c4ed9c537a90d3b7343f9f0a0430182
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-11 16:03:39 -0700

    Update rle.R

--- rle.R
+++ rle.R
@@ -1,4 +1,5 @@
-tripeaks = c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,0,1)
+tripeaks = c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,0,1)
 rle.tripeaks =rle(tripeaks)
 
 runs =rle.tripeaks$lengths
@@ -23,6 +24,32 @@ m/a # Average cycle length
 plot(x[1:100], type="b", xlab="Step", ylab="State")#plots shift in stochastic process. Lines should be vertical but are not
 # due to pixel limitations in R. Only outcomes are 0, below average bookings and 1, above average bookings
 
-sim_runs = rle(x[2:m] ==1)
-sim_runs
-average_length_1
+x[2]
+x[3]
+sim_runs = rle(x) #run length encoding, gives the length of the runs.
+length_vector = sim_runs$length #This vector gives the lengths of runs in a two state markov chain. 
+run_of_negative_days = 0
+i = 1
+repeat    #equivalent of a do while loop, breaks when there are no more runs of 0's
+{
+  if (i >1249)
+  {break     }
+  run_of_negative_days = run_of_negative_days + length_vector[i]
+  i = i+2
+}
+average_negative_run = run_of_negative_days / (length(length_vector)/2) 
+average_negative_run
+
+
+
+run_of_positive_days = 0
+i = 2
+repeat
+{
+  if (i >1250)
+  {break     }
+  run_of_positive_days = run_of_positive_days + length_vector[i]
+  i = i+2
+}
+run_of_positive_days
+average_positive_run = run_of_positive_days/625 (length(length_vector)/2)
