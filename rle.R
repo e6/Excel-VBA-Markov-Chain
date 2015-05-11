0aeabda95985082bd1867207d270bd59b01d7853
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-05-11 14:21:21 -0700

    Update rle.R

--- rle.R
+++ rle.R
@@ -3,4 +3,26 @@ rle.tripeaks =rle(tripeaks)
 
 runs =rle.tripeaks$lengths
 runs
-acf(tripeaks, plot=TRUE)
+acf(tripeaks, plot=TRUE, main= "Tripeaks Memory Plot")
+warnings()
+
+
+set.seed(1237)
+m = 10000; n = 1:m; x = numeric(m); x[1] = 0 #start with a below average bookings day
+alpha =.3333 ; beta = 0.07597
+# Simulation
+for (i in 2:m)
+{
+  if (x[i-1]==0) x[i] = rbinom(1, 1, alpha)
+  else x[i] = rbinom(1, 1, 1 - beta)
+}
+y = cumsum(x)/n # Running fractions of bookings
+y[m] # Fraction of above average bookings among m. Same as: mean(x)
+a = sum(x[2:m]==1); a #"x[1:(m-1)]==0 & "
+m/a # Average cycle length
+plot(x[1:100], type="b", xlab="Step", ylab="State")#plots shift in stochastic process. Lines should be vertical but are not
+# due to pixel limitations in R. Only outcomes are 0, below average bookings and 1, above average bookings
+
+sim_runs = rle(x[2:m] ==1)
+sim_runs
+average_length_1
